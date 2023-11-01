9102df9187087aca4426113e879c7489e9e8245a
Shailenkaushik <107068730+Shailenkaushik@users.noreply.github.com>
2023-11-01 18:11:29 +0530

    Navbar completed

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+
+import "./app.scss";
+
+
+
+import Navbar from "./components/navbar/Navbar";
+
+
+const App = () => {
+  return (
+    <div>
+     
+      <section id="Homepage">
+        <Navbar />
+        
+      </section>
+      <section id="Services">
+Parallax
+      </section>
+      <section>
+        Services
+      </section>
+      <section id="Portfolio">
+        Parallax
+      </section>
+     Portfolio
+      <section id="Contact">
+        Contact
+      </section>
+      {/* Framer Motion Crash Course */}
+      {/* <Test/>
+    <Test/> */}
+    </div>
+  );
+};
+
+export default App;
